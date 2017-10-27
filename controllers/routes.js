4986996107b69c9b972a2158badf222eb791f89c
Tyler Negro <tnegro@gmail.com>
2017-10-27 17:35:48 -0600

    updating routes

--- controllers/routes.js
+++ controllers/routes.js
@@ -18,6 +18,7 @@ module.exports = function(app){
         //delete existing non-saved articles first
         db.Article.remove({saved: false})
             .catch((error) => res.send(error))
+            // once deleted scrape the page
             .then(() => {
                 let newsURL = "http://www.gamespot.com/news/"
                 request(newsURL, (error, response, html) => {
@@ -32,14 +33,14 @@ module.exports = function(app){
                         let thumbnail = $(element).children('a').children('figure.media-figure').children('.media-img').children('img').attr('src');
                         isValidArticle(storyID, href, headline, summary, thumbnail, articleArray);
                     });
-
+                    // save scrape to the db
                     db.Article.create(articleArray)
                         .catch((error) => res.send(error))
                         .then((result) => res.redirect('/'))
                 });
             });  
     });
-
+    // route for the saved articles page
     app.get('/saved-articles', (req, res) =>{
         db.Article.find({saved: true}, (err, articles) =>{
             if(err){
@@ -59,6 +60,8 @@ module.exports = function(app){
     })
 
     app.post('/api/save-article', (req, res)=>{
+        //this needs to just update the article saved property to true. find article by id and then update true
+        
         db.Article.create(req.body, (error, result)=>{
             if(error){
                 console.log(error)

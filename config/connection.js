5e11e4efb24303d1d291b4072c7dabcde0b3bb7e
Tyler Negro <TylerNegro@Tylers-MacBook-Pro.local>
2017-10-27 08:14:02 -0600

    added env variables for heroku

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,8 @@
 
 module.exports = function (mongoose) {
     // Setup the Mongoose connections and DB
-    // let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-    mongoose.connect("mongodb://localhost/mongoHeadlines");
+    let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+    mongoose.connect(MONGODB_URI);
     
         let dbConnect = mongoose.connection;
         dbConnect.on("error", function (error) {
